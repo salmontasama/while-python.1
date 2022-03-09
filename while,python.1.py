bad2fdbd312b0327bca695669f4e950f3693a763
salmontasama <salmontasama1@gmail.com>
2022-03-09 21:54:10 +0200

    Answers while

--- while,python.1.py
+++ while,python.1.py
@@ -1,6 +1,33 @@
 ##1##
-i = 0
-while i <20:
-    i += 1
-    if i %2 !=0:
-     print(i)
+# i = 0
+# while i <20:
+#     i += 1
+#     if i %2 !=0:
+#      print(i)
+##2##
+# i = int(input("enter number"))
+# x = 0
+# while x <i:
+#     x +=1
+#     if x  %2 !=0:
+#      print(x)
+##3##
+# y = int(input("enter '1'or'2'"))
+# i = int(input("enter number"))
+# x = 0
+# z = 0
+# while x < i:
+#     x +=1
+#     z +=1
+#     if y == 1 and x %2 !=0:
+#
+#         print(x)
+#     elif y == 2 and x %2 == 0:
+#
+#         print(z)
+##3##
+
+
+##11##
+
+
